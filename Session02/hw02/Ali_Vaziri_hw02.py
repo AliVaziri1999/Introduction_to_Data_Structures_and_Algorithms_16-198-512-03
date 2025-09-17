40e3d891e562933c7879b44758182d2630081fe0
Ali Vaziri <vaziri1378@gmail.com>
2025-09-17 15:40:00 -0400

    updated some line of codes for hw02

--- Session02/hw02/Ali_Vaziri_hw02.py
+++ Session02/hw02/Ali_Vaziri_hw02.py
@@ -15,7 +15,7 @@ def toInt(s):
 # Q1 - Test case:
 print("Q1 - Test case:")
 test_string = "2375"
-print((test_string))
+print(toInt(test_string))
 ##############################
 
 # Question 2:
@@ -27,17 +27,17 @@ def sumString(n):
         return sumString(n - 1) + "+" + str(n)
 
 
-# Q2 - Test case:
-print("Q2 - Test case:")
-test_value = 5
-print(sumString(test_value))
+# # Q2 - Test case:
+# print("Q2 - Test case:")
+# test_value = 5
+# print(sumString(test_value))
 ##############################
 
 # Question 3:
 
 
-# Q3 - Test case:
-print("Q3 - Test case:")
+# # Q3 - Test case:
+# print("Q3 - Test case:")
 ##############################
 
 # Question 4:
@@ -48,10 +48,10 @@ def figure(n, current=1):
     figure(n, current + 1) # recursive call for the next row
 
 
-# Q4 - Test case:
-print("Q4 - Test case:")
-n = 5
-figure(n)
+# # Q4 - Test case:
+# print("Q4 - Test case:")
+# n = 5
+# figure(n)
 ##############################
 
 # Question 5:
@@ -65,8 +65,8 @@ def climbCount(n):
     return climbCount(n - 1) + climbCount(n - 2) + climbCount(n - 3) # Recursive; try step 1, 2, or 3.
 
 
-# Q5 - Test case:
-print("Q5 - Test case:")
-n = 4
-print(climbCount(n))
+# # Q5 - Test case:
+# print("Q5 - Test case:")
+# n = 4
+# print(climbCount(n))
 ##############################
