4fc91e9c1df03a863dbfb457d20c03372f0dff3d
Ali Vaziri <vaziri1378@gmail.com>
2025-09-17 15:14:15 -0400

    updated some line of codes for hw02

--- Session02/hw02/Ali_Vaziri_hw02.py
+++ Session02/hw02/Ali_Vaziri_hw02.py
@@ -64,8 +64,7 @@ def climbCount(n):
     if n < 0:
         return 0
 
-    # Recursive case: try 1, 2, or 3 steps
-    return climbCount(n - 1) + climbCount(n - 2) + climbCount(n - 3)
+    return climbCount(n - 1) + climbCount(n - 2) + climbCount(n - 3) # Recursive, calculate the number of ways to climb the staircase
 
 
 # Q5 - Test case:
