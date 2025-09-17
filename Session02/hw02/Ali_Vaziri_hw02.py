b4bef4f6b2297909a1ad234ccf786f8d4df2284a
Ali Vaziri <vaziri1378@gmail.com>
2025-09-17 14:17:19 -0400

    updated some self practice link of codes

--- Session02/hw02/Ali_Vaziri_hw02.py
+++ Session02/hw02/Ali_Vaziri_hw02.py
@@ -1,6 +1,9 @@
 # Author: Ali Vaziri
 # HW02
 
+##############################
+# Questions
+##############################
 # Question 1:
 def toInt(s):
 
@@ -10,16 +13,46 @@ def toInt(s):
         last_digit = ord(s[-1]) - ord('0')  # Get the last character and convert it to int value
         return toInt(s[:-1]) * 10 + last_digit # Recursive call for the remaining string
 
+##############################
+
+# Question 2:
+def sumString(n):
+
+    if n == 1: # base case
+        return "1"
+    else:
+        return sumString(n - 1) + "+" + str(n)
+
+##############################
+
+# Question 3:
+
+##############################
+
+# Question 4:
+
+
+##############################
 
 
 
+##############################
 # Test cases for the functions
+##############################
 if __name__ == "__main__":
 
     # Q1 - Test case:
     test_string = "2375"
     result = toInt(test_string)
+    print("Q1 - Test case:")
     print(result)
 
 
-    # Q2
+    # Q2 - Test case:
+    test_value = 5
+    result = sumString(test_value)
+    print("Q2 - Test case:")
+    print(result)
+
+
+    # Q3 - Test case:
