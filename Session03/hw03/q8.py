3117b2138c37f6e3836903e2030f3ae98b1c2f6e
Ali Vaziri <vaziri1378@gmail.com>
2025-09-24 13:27:50 -0400

    updated some line of codes for hw03, q8

--- Session03/hw03/q8.py
+++ Session03/hw03/q8.py
@@ -1,18 +1,41 @@
-def rearrange_iter(L, k):
-    i, j = 0, len(L) - 1
+# def rearrange_iter(L, k):
+#     i, j = 0, len(L) - 1
+#
+#     while i <= j:
+#         if L[i] < k: # element stays on left
+#             i += 1
+#         else:
+#             L[i], L[j] = L[j], L[i] # swap with right side
+#             j -= 1 # shrink right boundary
+#     return L
+#
+# # test case (same arrays from hw2 q3 to comparison)
+# print(rearrange_iter([3, 4, 8, 9, 5, 2], 8))
+# print(rearrange_iter([8, 8, 3, 4, 8, 9, 5, 2], 8))
+# print(rearrange_iter([8, 7, 6, 1, 2, 3, 4], 3))
+# '''
+# output:
+# [3, 4, 2, 5, 9, 8]
+# [2, 5, 3, 4, 9, 8, 8, 8]
+# [2, 1, 6, 7, 3, 4, 8]
+# '''
 
-    while i <= j:
-        if L[i] < k: # element stays on left
-            i += 1
+def rearrange_nonrecursive(S, k):
+    left, right = 0, len(S) - 1
+
+    while left <= right:
+        if S[left] < k: # element stays on left
+            left += 1
         else:
-            L[i], L[j] = L[j], L[i] # swap with right side
-            j -= 1 # shrink right boundary
-    return L
+            S[left], S[right] = S[right], S[left] # swap with right side
+            right -= 1
+    return S
+
 
 # test case (same arrays from hw2 q3 to comparison)
-print(rearrange_iter([3, 4, 8, 9, 5, 2], 8))
-print(rearrange_iter([8, 8, 3, 4, 8, 9, 5, 2], 8))
-print(rearrange_iter([8, 7, 6, 1, 2, 3, 4], 3))
+print(rearrange_nonrecursive([3, 4, 8, 9, 5, 2], 8))
+print(rearrange_nonrecursive([8, 8, 3, 4, 8, 9, 5, 2], 8))
+print(rearrange_nonrecursive([8, 7, 6, 1, 2, 3, 4], 3))
 '''
 output:
 [3, 4, 2, 5, 9, 8]
