4e7e67bae56bad30ed57d1ee5a1954357bcd7748
Ali Vaziri <vaziri1378@gmail.com>
2025-10-02 10:56:46 -0400

    update hw04

--- Session04/hw04/ali_vaziri_hw04.py
+++ Session04/hw04/ali_vaziri_hw04.py
@@ -4,48 +4,43 @@
 # Fall 2025
 # Author: Ali Vaziri
 
-
-# 1- return the number of nodes by going the links recursively
+# 1. return the number of nodes by recursion
 def size(self):
-
     def _count(node):
-        if node is None: # base case --> no node
+        if node is None:
             return 0
-
-        return 1 + _count(node._next) # recursive case --> 1 (this node) + the rest size
+        return 1 + _count(node._next)
 
     return _count(self._head)
 
 
-# 2- reverse the list by flipping the "next" pointers
+# 2. reverse the list in place
 def reverse(self):
 
     prev = None
     curr = self._head
-    self._tail = self._head # after reversal the old head becomes the new tail
+
+    self._tail = self._head # old head will become the new tail
 
     while curr is not None:
-        next = curr._next # save next node before we overwrite the link
-        curr._next = prev # flip the arrow
-        prev = curr # step prev forward
-        curr = next # step curr forward
-        # prev now points to the old tail, which is the new head
+        nxt = curr._next # remember next node
+        curr._next = prev # flip link
+        prev = curr
+        curr = nxt
 
-    self._head = prev
+    self._head = prev # prev is the old tail (new head)
 
 
-# 3- remove duplicates from linked list
+# 3. remove duplicates from a sorted list
 def removeDuplicates(self):
 
     curr = self._head
+    while curr is not None and curr._next is not None:
+        if curr._element == curr._next._element: # skip the duplicate node
 
-    while curr is not None and curr._next is not None: # walk in the list and comparing each node
-        if curr._element == curr._next._element:# duplicate found, unlink the next node
-            dup = curr._next
-            curr._next = dup._next
-            self._size -= 1 # stored size consistent
-
-            if curr._next is None: # update tail because removed the last node
+            curr._next = curr._next._next
+            self._size -= 1
+            if curr._next is None: # we removed the last node
                 self._tail = curr
         else:
-            curr = curr._next # move forward no duplicate
+            curr = curr._next
