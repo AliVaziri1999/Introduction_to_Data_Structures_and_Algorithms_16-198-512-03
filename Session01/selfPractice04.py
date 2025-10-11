c316a09e6ad591eb0fc40ffa015787dd404a4304
Ali Vaziri <vaziri1378@gmail.com>
2025-10-11 14:40:49 -0400

    adding some python files practice week 1

--- Session01/selfPractice04.py
+++ Session01/selfPractice04.py
@@ -22,31 +22,55 @@
 '''
         Queue
 '''
-from queue import Queue
+# from queue import Queue
+#
+# q = Queue()
+# # print(q.qsize())
+# # Peek at contents (without losing them)
+# items = list(q.queue)   # internal deque object
+# print(items)             # [5, 10, 15]
+#
+# q.put(5)   # enqueue
+# # Peek at contents (without losing them)
+# items = list(q.queue)   # internal deque object
+# print(items)             # [5, 10, 15]
+#
+# q.put(2)   # enqueue
+# # Peek at contents (without losing them)
+# items = list(q.queue)   # internal deque object
+# print(items)             # [5, 10, 15]
+#
+# a = q.get()    # dequeue
+# # Peek at contents (without losing them)
+# items = list(q.queue)   # internal deque object
+# print(items)             # [5, 10, 15]
+#
+# b = a * 2
+# print(b)
 
-q = Queue()
-# print(q.qsize())
-# Peek at contents (without losing them)
-items = list(q.queue)   # internal deque object
-print(items)             # [5, 10, 15]
+'''
+        Deque
+'''
 
-q.put(5)   # enqueue
-# Peek at contents (without losing them)
-items = list(q.queue)   # internal deque object
-print(items)             # [5, 10, 15]
-
-q.put(2)   # enqueue
-# Peek at contents (without losing them)
-items = list(q.queue)   # internal deque object
-print(items)             # [5, 10, 15]
-
-a = q.get()    # dequeue
-# Peek at contents (without losing them)
-items = list(q.queue)   # internal deque object
-print(items)             # [5, 10, 15]
-
-b = a * 2
-print(b)
+# from collections import deque
+# d = deque()
+# # print(d)
+# d.append(1)       # add right
+# # print(d)
+# d.append(2)
+# d.append(3)
+# # print(d)
+#
+# d.appendleft(0)   # add left
+# # print(d)
+# d.appendleft(-1)
+# print(d)
+#
+#
+# d.pop()           # remove right
+# print(d)
+# d.popleft()       # remove left
+# print(d)
 
 '''
 
